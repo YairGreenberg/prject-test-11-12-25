b98e14f865350db4c4bb776509a06eb0c46a6010
yair greenberg <yair290899@gmail.com>
2025-12-11 12:59:55 +0200

    finish project

--- utils/changePrice.js
+++ utils/changePrice.js
@@ -1,15 +1,16 @@
 import { OperateOnStock } from "../server/upDatePrice.js";
 import { stockMarket } from "../db/data.js";
 
+export let changFromPrice = (up_date, buyStock) =>
+  up_date.category.map((element) => {
+    if (element !== buyStock) {
+      element.currentPrice += element.currentPrice / 100;
+    }
+  });
 
-
-
-export let changFromPrice = (up_date,buyStock)=>up_date.category.map((element) =>
-   {if(element !== buyStock){
-        element.currentPrice += element.currentPrice /100
-   }});
-
-export let changFromPricedwon = (up_date,buyStock)=>up_date.category.map((element) =>
-   {if(element !== buyStock){
-        element.currentPrice -= element.currentPrice /100
-   }});
+export let changFromPricedwon = (up_date, buyStock) =>
+  up_date.category.map((element) => {
+    if (element !== buyStock) {
+      element.currentPrice -= element.currentPrice / 100;
+    }
+  });
